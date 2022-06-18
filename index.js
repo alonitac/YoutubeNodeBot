4edf6d320dee0d897c4f02e3e618bdb20b0cff2f
alonit <alon.itach@mail.huji.ac.il>
2022-06-18 19:48:21 +0300

    add logs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ytsr = require('ytsr');
 
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '5265428664:AAEUn4aDxkSdCARqIBA0EYxaEGQdKtGoLtM';
+const token = fs.readFileSync('.token');
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -15,20 +15,21 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
+    console.log(`Incoming message: ${msg.text}`);
+
     bot.sendMessage(chatId, 'Downloading....');
 
     ytsr(msg.text, {limit: 1, pages: 1}).then((result) => {
-        // console.log(result);
+        console.log(result.items[0]);
 
         if (result.items.length > 0){
             const filename = `${result.items[0].id}.mp4`;
             ytdl(result.items[0].url)
                 .pipe(fs.createWriteStream(filename)).on('finish', () => {
-
+                    console.log(`File downloaded: ${filename}`);
                     bot.sendMessage(chatId, `${result.items[0].title} Downloaded: \n\n ${result.items[0].url}`);
             });
         } else {
-
             bot.sendMessage(chatId, `No video found for ${msg.text}... `);
         }
 
